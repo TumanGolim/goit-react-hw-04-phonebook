e577f583d6633338c01408550271cbe55ea51c72
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-12-23 14:46:35 +0200

    comp

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ContactList extends Component {
-  render() {
-    const { filteredContacts, deleteContact } = this.props;
-
-    return (
-      <ul>
-        {filteredContacts.map(contact => (
-          <li key={contact.id}>
-            {contact.name}: {contact.number}
-            <button type="button" onClick={() => deleteContact(contact.id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const ContactList = ({ filteredContacts, deleteContact }) => (
+  <ul>
+    {filteredContacts.map(contact => (
+      <li key={contact.id}>
+        {contact.name}: {contact.number}
+        <button type="button" onClick={() => deleteContact(contact.id)}>
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default ContactList;
